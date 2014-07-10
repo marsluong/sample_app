d9926342207d8379cb668d4b291af092efe6799e
marsluong <mariavyl_10491@yahoo.com>
2014-07-10 11:45:12 -0500

    add_index_to_users_email.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,16 @@ describe User do
 		it { should_not be_valid } 
 	end
 
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it { should_not be_valid }
+	end
+
 	describe "when email is not present" do 
 		before { @user.email = " " }
 		it { should_not be_valid }	
@@ -26,7 +36,7 @@ describe User do
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
-			address.each do |invalid address|
+			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
 			end
@@ -36,9 +46,9 @@ describe User do
 	describe "when email format is valid" do
 		it "should be valid" do
 			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-			addresses.each do |valid address|
+			addresses.each do |valid_address|
 				@user.email = valid_address
-				expect(@user.to be_valid
+				expect(@user).to be_valid
 			end
 		end
 	end
